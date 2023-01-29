13635186784219c6754ae129ca12f07b75b72f54
zmoussam <zakariamoussammi87@gmail.com>
2023-01-29 22:12:59 +0100

    gg

---
+++ cpp_00/ex01/contact.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zmoussam <zmoussam@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/29 21:35:36 by zmoussam          #+#    #+#             */
+/*   Updated: 2023/01/29 22:00:01 by zmoussam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __CONTACT__
+    #define __CONTACT__
+
+#include<iostream>
+
+class contact
+{
+    public:
+        contact(void);   
+        ~contact(void);
+        std::string arr;   
+};
+
+#endif
