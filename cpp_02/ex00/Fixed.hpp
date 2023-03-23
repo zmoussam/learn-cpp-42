a06b842a2a5f48b32ec2819f1ff09ca398a76055
zmoussam <zakariamoussammi87@gmail.com>
2023-03-23 00:46:15 +0000

    start cpp_02

---
+++ cpp_02/ex00/Fixed.hpp
@@ -0,0 +1,16 @@
+#ifndef __FIXED_H
+#define __FIXED_H
+
+#include <iostream>
+
+class Fixed{
+    private:
+        int FixedPoint;
+        static const int FractionalBits;
+    public:
+        Fixed();
+        
+
+};
+
+#endif
