0e6cdb0329a10a79ece785eec85d92acd50cafdd
zmoussam <zakariamoussammi87@gmail.com>
2023-03-27 14:51:31 +0000

    ex00 cpp02

--- cpp_02/ex00/Fixed.cpp
+++ cpp_02/ex00/Fixed.cpp
@@ -9,9 +9,27 @@ Fixed::Fixed(){
 
 Fixed::Fixed(const Fixed &copy){
     *this = copy;
-    std::cout << "Copy assignment operator called" << std::endl; 
+    std::cout << "Copy constructor called" << std::endl;
 }
 
-Fixed &Fixed::operator=(const Fixed &src){
-    if
+Fixed &Fixed::operator =(const Fixed &src){
+    
+    std::cout << "Copy assignment operator called" << std::endl; 
+    this->FixedPoint = src.getRawBits();
+
+    return *this;
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits(void) const{
+    std::cout << "getRawBits member function called" << std::endl;
+    return this->FixedPoint;
+}
+
+void Fixed::setRawBits(int const raw){
+    this->FixedPoint = raw;
 }
